68666c8c5fe0b200eb493b5429506e7df71dc99c
Josh Rebelez <59lpjrebelez@gmail.com>
2019-06-07 12:14:14 -0700

    hello world

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Main from './Components/MainComponent';
 import './App.css';
 
 function App() {
---
+++ src/Components/MainComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    
+    }
+
+    render(){
+     return(
+    <p> hello world </p>
+     )          
+}
+}
+
+
+export default Main;
