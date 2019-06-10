711296791743c72052c315b2d0cf4691a12764f8
Josh Rebelez <59lpjrebelez@gmail.com>
2019-06-10 12:10:14 -0700

    home.js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 import './App.css';
 
-function App() {
-  return (
-    <Navbar/>
-  );
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                   
+                  </Switch>
+             </div>
+       </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
