a92f90b9aeb279e6436b53677f9e5ca3e52c143c
Josh Rebelez <59lpjrebelez@gmail.com>
2019-06-20 12:28:27 -0700

    build

---
+++ build/precache-manifest.f9cc9c18a13995bce7e33d4d8107c82f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977bbd701f71ee93049246891805380b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b60e1c7b6d39acd50b3",
+    "url": "/static/css/2.b7c6c5e7.chunk.css"
+  },
+  {
+    "revision": "e8ffea860b1afe44ffd5",
+    "url": "/static/css/main.2921bcae.chunk.css"
+  },
+  {
+    "revision": "2b60e1c7b6d39acd50b3",
+    "url": "/static/js/2.6f92b9c9.chunk.js"
+  },
+  {
+    "revision": "e8ffea860b1afe44ffd5",
+    "url": "/static/js/main.a110f140.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "def813c3a82a6f8b57ff45a706190e99",
+    "url": "/static/media/item1.def813c3.png"
+  },
+  {
+    "revision": "79079c741b9e20cc12db04ec32518cbc",
+    "url": "/static/media/item2.79079c74.png"
+  },
+  {
+    "revision": "ae63079020fa059fdb692c7df50e39d5",
+    "url": "/static/media/item3.ae630790.jpg"
+  },
+  {
+    "revision": "21b5a5369c061d9feb97b491d4b8589a",
+    "url": "/static/media/item4.21b5a536.png"
+  },
+  {
+    "revision": "80bc91d910150cd2a5d83d530ab7ba9f",
+    "url": "/static/media/item5.80bc91d9.jpg"
+  },
+  {
+    "revision": "ef21bdd8dacb1e9cc820f2eae81157a4",
+    "url": "/static/media/item6.ef21bdd8.jpg"
+  },
+  {
+    "revision": "1bc99912f9ffc99e99fa681ed55fb59a",
+    "url": "/static/media/logo.1bc99912.png"
+  }
+]);
